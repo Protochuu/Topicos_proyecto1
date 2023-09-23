20800e37c2f2d2440315f7ef39c695bb7f2e00ce
duskje <duskje@protonmail.com>
2023-09-23 01:00:19 -0300

    test: probar murmurhash64

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <MurmurHash3.h>
+
+#include <iostream>
+
+int main(){
+	const char *key = "Hola";
+
+	uint64_t out;
+
+	MurmurHash3_x64_128(key, 4, 0, &out);
+
+	std::cout << out;
+	
+	return 0;
+}
