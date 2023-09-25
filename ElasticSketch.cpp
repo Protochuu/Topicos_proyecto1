85290c44377bb067f505299938ad47278b80bdc4
protochuu <chat.et.coq@gmail.com>
2023-09-25 18:57:15 -0300

    add implementation ElasticSketch.cpp

--- ElasticSketch.cpp
+++ ElasticSketch.cpp
@@ -4,8 +4,57 @@
 #include "ElasticSketch.h"
 
 using namespace std;
+
+template <size_t B>
+void ElasticSketchHashTable<B>::insert(string element){
+    
+    int out;
+    int threshold = 8;
+    MurmurHash3_x64_128(element.c_str(), element.size(), 0, &out);
+    int hash_index = out%B;
+    if (hash_table.is_empty) {
+        hash_table[hash_index] = element.element;
+        hash_table.positive_count = 1;
+    } 
+    else{
+        if (hash_table.element == element.element){
+            hash_table.positive_count += 1;
+        }
+        else {
+            hash_table.negative_count += 1;
+            ratio = hash_table.negative_count/hash_table.positive_count;
+            if (ratio < threshold){
+                c.increment_count
+            }
+        }
+    }
+
+
+}
+
 template <size_t W, size_t D>
 void ElasticSketch<W, D>::increment_count(string element){
+        int out;
+    int threshold = 8;
+    MurmurHash3_x64_128(element.c_str(), element.size(), 0, &out);
+    int hash_index = out%B;
+    if (hash_table.is_empty) {
+        hash_table[hash_index] = element.element;
+        hash_table.positive_count = 1;
+    } 
+    else{
+        if (hash_table.element == element.element){
+            hash_table.positive_count += 1;
+        }
+        else {
+            hash_table.negative_count += 1;
+            ratio = hash_table.negative_count/hash_table.positive_count;
+            if (ratio < threshold){
+                count_min_sketch.increment_count(hash_table.element);
+
+            }
+        }
+    }
 
 }
 
--- ElasticSketch.h
+++ ElasticSketch.h
@@ -7,6 +7,7 @@ struct ElasticSketchBucket {
 	std::string element;
 	uint64_t positive_count = 0;
 	bool replaced = false;
+	bool is_empty = true;
 	uint64_t negative_count = 0;
 };
 
@@ -15,12 +16,13 @@ class ElasticSketchHashTable {
 private:
 	std::array<ElasticSketchBucket, B> hash_table;
 public:
-	void insert(ElasticSketchBucket element);
+	void insert(std::string element);
 };
 
 template<size_t W, size_t D>
 class ElasticSketch {
 private:
+	std::array<ElasticSketchBucket, B> hash_table;
 	ElasticSketchHashTable<std::string, ElasticSketchBucket, W> table;
 	CountMinCU<W, D> count_min_sketch;
 
