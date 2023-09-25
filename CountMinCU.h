fc760be53c8861c8ac625cba0a3910b6cd6cd34c
duskje <duskje@protonmail.com>
2023-09-25 20:55:35 -0300

    final header

--- CountMinCU.h
+++ CountMinCU.h
@@ -2,11 +2,13 @@
 #include <vector>
 #include <string>
 
+#include "Sketch.h"
+
 #ifndef COUNTMINCU
 #define COUNTMINCU
 
 template <size_t W, size_t D>
-class CountMinCU {
+class CountMinCU : Sketch {
 private:
 	std::vector<std::vector<uint64_t>> freqs;
 	uint32_t rows = D;
@@ -14,8 +16,8 @@ private:
 
 	uint64_t hash(std::string key, int seed);
 public:
-	void increment_count(string element);
-	uint64_t retrieve_count(string element);
+	void increment_count(string element) override;
+	uint64_t retrieve_count(string element) override;
     void set_count(std::string element, uint64_t count);
 };
 
