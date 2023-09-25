11bbe9aa53a4251f7f4a591be0be0f1a7f3f63de
protochuu <chat.et.coq@gmail.com>
2023-09-25 20:24:05 -0300

    Implementation ElasticSketch.cpp and fix errors

--- ElasticSketch.cpp
+++ ElasticSketch.cpp
@@ -5,60 +5,52 @@
 
 using namespace std;
 
-template <size_t B>
-void ElasticSketchHashTable<B>::insert(string element){
-    
+template <size_t W, size_t D>
+ElasticSketchBucket *ElasticSketch<W,D>::get_bucket(string element){
     int out;
-    int threshold = 8;
     MurmurHash3_x64_128(element.c_str(), element.size(), 0, &out);
-    int hash_index = out%B;
-    if (hash_table.is_empty) {
-        hash_table[hash_index] = element.element;
-        hash_table.positive_count = 1;
-    } 
-    else{
-        if (hash_table.element == element.element){
-            hash_table.positive_count += 1;
-        }
-        else {
-            hash_table.negative_count += 1;
-            ratio = hash_table.negative_count/hash_table.positive_count;
-            if (ratio < threshold){
-                c.increment_count
-            }
-        }
-    }
-
-
+    int hash_index = out%W;
+    return &hash_table[hash_index];
 }
 
 template <size_t W, size_t D>
 void ElasticSketch<W, D>::increment_count(string element){
-        int out;
     int threshold = 8;
-    MurmurHash3_x64_128(element.c_str(), element.size(), 0, &out);
-    int hash_index = out%B;
-    if (hash_table.is_empty) {
-        hash_table[hash_index] = element.element;
-        hash_table.positive_count = 1;
+    ElasticSketchBucket bucket = get_bucket(element);
+    if (bucket->is_empty){
+        bucket->element= element;
+        bucket->positive_count = 1;
+        bucket->is_empty = false;
     } 
-    else{
-        if (hash_table.element == element.element){
-            hash_table.positive_count += 1;
+    else if (bucket->element == element){
+        bucket->positive_count += 1;
+    }
+    else {
+        bucket->negative_count += 1;
+        double ratio = bucket->negative_count/bucket->positive_count;
+        if (ratio < threshold){
+            count_min_sketch.increment_count(bucket->element);
         }
         else {
-            hash_table.negative_count += 1;
-            ratio = hash_table.negative_count/hash_table.positive_count;
-            if (ratio < threshold){
-                count_min_sketch.increment_count(hash_table.element);
-
-            }
+            count_min_sketch.set_count(bucket->element,bucket->positive_count);
+            bucket->element = element;
+            bucket->positive_count = 1;
+            bucket->replaced = true;
+            bucket->negative_count = 1;
         }
     }
-
 }
 
 template <size_t W, size_t D>
 uint64_t ElasticSketch<W, D>::retrieve_count(string element){
-
+    ElasticSketchBucket bucket = get_bucket(element);
+    if ((bucket->is_empty) && (bucket->element == element)){
+        return count_min_sketch.retrieve_count(element);
+    }
+    else if(bucket->replaced){
+        return bucket->positive_count + count_min_sketch.retrieve_count(element);
+    }
+    else{
+        return bucket->positive_count;
+    }
 }
--- ElasticSketch.h
+++ ElasticSketch.h
@@ -11,14 +11,6 @@ struct ElasticSketchBucket {
 	uint64_t negative_count = 0;
 };
 
-template<size_t B>
-class ElasticSketchHashTable {
-private:
-	std::array<ElasticSketchBucket, B> hash_table;
-public:
-	void insert(std::string element);
-};
-
 template<size_t W, size_t D>
 class ElasticSketch {
 private:
@@ -29,6 +21,7 @@ private:
 public:
 	void increment_count(std::string element);
 	uint64_t retrieve_count(std::string element);
+	ElasticSketchBucket *get_bucket(std::string element);
 };
 
 
