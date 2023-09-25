4ad28b2b30fe5ad423d0771727a5ac2f52a9618d
protochuu <chat.et.coq@gmail.com>
2023-09-25 16:02:05 -0300

    add ElasticSketch.cpp

---
+++ ElasticSketch.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+#include "CountMinCU.h"
+#include "ElasticSketch.h"
+
+using namespace std;
+
+void ElasticSketch::increment_count(std::string element){
+
+}
+uint64_t ElasticSketch::retrieve_count(std::string element){
+
+}
